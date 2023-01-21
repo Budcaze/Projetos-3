87a5c69a687edba21f4372404bc205e82eec47a7
MarcosNeto14 <marcosneto01@hotmail.com>
2023-01-20 21:28:40 -0300

    Adição da coluna Velocidade da via
    Tratou a coluna removendo as strings que poderiam impactar na km/h, remoção dos valores nulos e conversão da coluna string para int

---
+++ pages/Velocidade da via.py
@@ -0,0 +1,159 @@
+import pandas as pd
+import streamlit as st
+import altair as alt
+
+st.set_page_config(page_title="Acidentes Recife", # Configuração do setpage, ou seja, informações da página.
+    page_icon=":warning:",
+    layout="wide")
+df = pd.read_parquet("data/DataSetAcidentesRecife.parquet") # Abertura do DataSet.
+
+st.title("Velocidade da via")
+
+#Remover o Km/h da coluna velocidade_max_via
+df['velocidade_max_via'].replace({' km/h':''},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'KM/H':''},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'km':''},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'KM':''},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'N/I':''},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'n/i':''},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'/h':''},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'30 e 40':'35'},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'':'0'},regex=True,inplace=True)
+
+
+#Alterando tipo da coluna para inteiro
+df['velocidade_max_via'] = df['velocidade_max_via'].fillna(0) # substitui o vazio por 0
+df['velocidade_max_via'] = df['velocidade_max_via'].astype('int64') # Converte para inteiro
+st.dataframe(df['velocidade_max_via'].unique())
+
+# Converte o tipo das colunas para inteiro
+df['vitimas'] = df['vitimas'].fillna(0) # substitui o vazio por 0
+df['vitimas'] = df['vitimas'].astype('int64') # Converte para inteiro
+
+df['vitimasfatais'] = df['vitimasfatais'].fillna(0) # substitui o vazio por 0
+df['vitimasfatais'] = df['vitimasfatais'].astype('int64') # Converte para inteiro
+
+# Converte o ano para string
+df['Ano'] = df['Ano'].astype('str') #Converte para string
+
+# SIDEBAR
+st.sidebar.header("Filtre aqui por ano:") #Cada SIDEBAR é composto pelo cabeçalho seguido da nossa variavel que vai armazenar o filtro e eu posso colocar as opções de acordo com o meu dataset e um padrão para começar o filtro, nesse caso eu coloquei para pegar todos as opções que tem nas colunas
+ano = st.sidebar.multiselect(
+    "Selecione o ano: ",
+    options=df["Ano"].unique(),
+    default=df["Ano"].unique()
+)
+
+
+
+st.sidebar.header("Filtre aqui por bairro :")
+bairro = st.sidebar.multiselect(
+    "Selecione o bairro: ",
+    options=df["bairro"].unique(),
+    default=df["bairro"].unique()
+)
+
+
+
+df_selection = df.query( #Aqui eu vou atribuir a varivavel que eu criei nos sidebars as colunas do dataset
+    "Ano == @ano & bairro == @bairro" #O @ significa que estou chamando a varivel que criei lá no sidebar
+)
+st.dataframe(df_selection) #Aqui eu chamo nosso dataset para ele aparecer
+
+
+
+##### Gráficos #####
+
+#Vítimas fatais por ano  (Interativo)
+df_selection.rename(columns={'vitimasfatais':'Vítimas Fatais'}, inplace=True) # altera o nome da coluna
+df_selection.rename(columns={'acidente_verificado':'Localização na via'}, inplace=True) # altera o nome da coluna
+df_selection.rename(columns={'condicao_via':'Condição da via'}, inplace=True) # altera o nome da coluna
+df_selection.rename(columns={'vitimas':'Número de vítimas'}, inplace=True) # altera o nome da coluna
+df_selection.rename(columns={'bairro':'Bairro'}, inplace=True) # altera o nome da coluna
+df_selection.rename(columns={'tempo_clima':'Clima'}, inplace=True) # altera o nome da coluna
+
+VitimasTotais = df_selection.groupby(['Ano'])['Número de vítimas'].sum()
+VitimasTotais = VitimasTotais.reset_index()
+
+
+#Valores totais de acidentes por ano (quantidade de vezes que cada ano aparece no dataset)
+
+lista = df.groupby('Ano')['Ano'].transform('count')
+st.write(lista) #teste visual comrpovando número de vezes que x Ano está rpesente
+ano2015 = lista.loc[0] #linha 0 (inicial que é 2015) nessa 'lista' feita com o groupby
+indiceDeComeco = ano2015 #indiceDeComeco seta em que linha começará o próximo ano
+ano2016 = lista.loc[indiceDeComeco] #linha buscada de acordo com fim da contagem dos anos anteriores
+indiceDeComeco += ano2016
+ano2017 = lista.loc[indiceDeComeco]
+indiceDeComeco += ano2017
+ano2018 = lista.loc[indiceDeComeco]
+indiceDeComeco += ano2018
+ano2019 = lista.loc[indiceDeComeco]
+indiceDeComeco += ano2019
+ano2020 = lista.loc[indiceDeComeco]
+indiceDeComeco += ano2020
+ano2021 = lista.loc[indiceDeComeco]
+
+st.write('Ano 2015: ', ano2015, 'Ano 2016: ', ano2016, 'Ano 2017: ', ano2017, 'Ano 2018: ', 
+ano2018, 'Ano 2019: ', ano2019, 'Ano 2020: ', ano2020, 'Ano 2021: ', ano2021) #demonstração valores encontrados
+
+
+# Acidentes x Clima
+VitimasTempoAno = df_selection.groupby('Clima')['Número de vítimas'].sum()
+VitimasTempoAno = VitimasTempoAno.reset_index() # transforma o index em uma coluna
+
+pie_chart = alt.Chart(VitimasTempoAno).mark_arc().encode(
+    theta=alt.Theta(field="Número de vítimas", type="quantitative"),
+    color=alt.Color(field="Clima", type="nominal"),
+).properties(   # propriedades do gráfico
+    title='Quantidade de Vitimas x Clima' # adiciona o titulo no gráfico
+).configure_title(  # formata o titulo
+    fontSize = 20,
+    anchor= 'middle',   # centraliza o titulo
+    color= 'black'
+) 
+st.altair_chart(pie_chart, use_container_width=True)
+
+
+
+# Vítimas x Condição da Via (Interativo)
+VitimasCondicaoVia = df_selection.groupby('Condição da via')['Número de vítimas'].sum()
+VitimasCondicaoVia = VitimasCondicaoVia.reset_index() # transforma o index em uma coluna
+
+pie_chart = alt.Chart(VitimasCondicaoVia).mark_arc().encode(
+    theta=alt.Theta(field="Número de vítimas", type="quantitative"),
+    color=alt.Color(field="Condição da via", type="nominal"),
+).properties(   # propriedades do gráfico
+    title='Vítimas x Condição da Via' # adiciona o titulo no gráfico
+).configure_title(  # formata o titulo
+    fontSize = 20,  # tamanho da fonte
+    anchor= 'middle',   # centraliza o titulo
+    color= 'black'  # cor do titulo
+)  
+st.altair_chart(pie_chart, use_container_width=True)
+
+# Vítimas x Bairro (Interativo)
+VitimasBairro = df_selection.groupby('Bairro')['Número de vítimas'].sum()
+VitimasBairro = VitimasBairro.reset_index()
+bar_chart = alt.Chart(VitimasBairro).mark_bar(color='#7FFFD4').encode(
+    y= alt.Y('Bairro', sort='-x'), # sort='-x' ordena Y em ordem decrescente de acordo com os valores do eixo X
+    x= 'Número de vítimas',
+    
+).properties(   # propriedades do gráfico
+    title='Vítimas por Bairro' # adiciona o titulo no gráfico
+).configure_title(  # formata o titulo
+    fontSize = 20,  # tamanho da fonte
+    anchor= 'middle',   # centraliza o titulo
+    color= 'black'  # cor do titulo
+)  
+st.altair_chart(bar_chart, use_container_width=True)
+
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
