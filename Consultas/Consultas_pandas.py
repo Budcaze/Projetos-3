dd551b73c588726a823304917069bde98a78ace6
Caio Filipe <caio_filipe064@hotmail.com>
2023-01-30 19:54:42 -0300

    Criação página de veículos + Nova consulta
    A consulta adicionada é em razão de estarmos com um problema na plotagem de dados com o Clima como fator de filtragem, usar a função count() ao invés da sum() aparenta ser a solução.

--- Consultas/Consultas_pandas.py
+++ Consultas/Consultas_pandas.py
@@ -3,24 +3,27 @@ import pandas as pd
 df = pd.read_parquet("data/DataSetAcidentesRecife.parquet")
 df.rename(columns={'auto':'carro'}, inplace=True) # Alteração de nome de coluna
 df.rename(columns={'acidente_verificado':'Localização na via'}, inplace=True)
+df.rename(columns={'tempo_clima':'Clima'}, inplace=True)
 
     # Quantitativo anual de vitimas e vitimas fatais. Filtro = Ano
-print(df.groupby(['Ano']).sum()[['vitimas','vitimasfatais']].sort_values(by='Ano'))
+# print(df.groupby(['Ano']).sum()[['vitimas','vitimasfatais']].sort_values(by='Ano'))
 
     # Quantitativo anual de acidentes por tipo de veiculo. Filtro = Veiculo
-print(df.groupby(['Ano']).sum()[['moto', 'carro', 'caminhao','onibus']].sort_values(by='Ano'))
+# print(df.groupby(['Ano']).sum()[['moto', 'carro', 'caminhao','onibus']].sort_values(by='Ano'))
 
     # Quantitativo anual de acidentes por tipo de veiculo. Filtro = Condição da via
-print(df.groupby(['Ano', 'condicao_via']).sum()[['moto', 'carro', 'caminhao','onibus']].sort_values(by='Ano'))
+# print(df.groupby(['Ano', 'condicao_via']).sum()[['moto', 'carro', 'caminhao','onibus']].sort_values(by='Ano'))
 
     # Quantitativo anual de acidentes com vitimas por Bairro. Filtro = Bairro
 print(df.groupby(['bairro']).sum()[['vitimasfatais']].sort_values(by='bairro'))
 
     # Quantitativo anual de acidentes com vitimas por região da via. Filtro = Localização na via
-print(df.groupby(['Localização na via']).sum()[['vitimas']].sort_values(by='Localização na via'))
+# print(df.groupby(['Localização na via']).sum()[['vitimas']].sort_values(by='Localização na via'))
 
 """
     Usar resultados das respectivas consultas acima para validar resultados apresentados no gráficos
 """
 
-print(df.groupby(['Ano']).sum()[['tempo_clima']].sort_values(by='Ano'))
+# print(df.groupby(['Ano', 'Clima']).count().sort_values(by='Ano'))
+# print(df.groupby(['Clima']).sum(numeric_only=False))
+# print(df.columns)
