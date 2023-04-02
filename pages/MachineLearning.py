e0f5e011715b37005bfac505ba7234d98fc243b2
Igor Fernandes <igormdfernandes@gmail.com>
2023-04-02 18:47:28 -0300

    machinelearning

---
+++ pages/MachineLearning.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import streamlit as st
+import altair as alt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_absolute_error
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+
+# Carregando os dados
+df = pd.read_parquet("data/DataSetAcidentesRecife.parquet")
+
+df['Ano'] = df['Ano'].fillna(0)
+df['auto'] = df['auto'].fillna(0)
+df['moto'] = df['moto'].fillna(0)
+df['ciclom'] = df['ciclom'].fillna(0)
+df['ciclista'] = df['ciclista'].fillna(0)
+df['onibus'] = df['onibus'].fillna(0)
+df['caminhao'] = df['caminhao'].fillna(0)
+df['viatura'] = df['viatura'].fillna(0)
+df['outros'] = df['outros'].fillna(0)
+df['vitimas'] = df['vitimas'].fillna(0)
+df['vitimasfatais'] = df['vitimasfatais'].fillna(0)
+df['velocidade_max_via'] = df['velocidade_max_via'].fillna(0)
+#Remover o Km/h da coluna velocidade_max_via
+df['velocidade_max_via'].replace({' km/h':''},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'KM/H':''},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'km':''},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'KM':''},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'N/I':''},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'n/i':''},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'/h':''},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'30 e 40':'35'},regex=True,inplace=True)
+df['velocidade_max_via'].replace({'':'0'},regex=True,inplace=True)
+
+
+# Dividindo os dados em treinamento e teste
+newDF = df[['Ano', 'auto', 'moto', 'ciclom', 'ciclista', 'onibus', 'caminhao', 'viatura', 'outros', 'vitimas', 'vitimasfatais', 'velocidade_max_via']]
+X = newDF.drop(['vitimas'], axis=1)
+y = df['vitimas']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Criando um modelo de regressão linear com Scikit-Learn
+reg = LinearRegression()
+reg.fit(X_train, y_train)
+
+# Avaliando o desempenho do modelo com métricas de regressão
+y_pred = reg.predict(X_test)
+r2 = r2_score(y_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
+st.text(f'R²: {r2:.2f}, MAE: {mae:.2f}')
+
+scatter = alt.Chart(df).mark_point().encode(x='vitimas', y='velocidade_max_via')
+st.altair_chart(scatter, use_container_width=True)
+
+
+newDF2 = df[['Ano', 'vitimas', 'vitimasfatais', 'velocidade_max_via']]
+A = newDF2.drop(['vitimas'], axis=1)
+b = df['vitimas']
+A_train, A_test, b_train, b_test = train_test_split(A, b, test_size=0.2, random_state=42)
+
+# Criando um modelo de classificação com Random Forest
+clf = RandomForestClassifier()
+clf.fit(A_train, b_train)
+
+# Avaliando o desempenho do modelo com métricas de classificação
+b_pred = clf.predict(A_test)
+report2 = classification_report(b_test, b_pred)
+st.text(report2)
+
+gravidade_chart = alt.Chart(df).mark_bar().encode(
+    x=alt.X('gravidade', bin=True),
+    y='count()',
+    color=alt.Color('gravidade')
+).properties(
+    width=600,
+    height=400
+)
+
+#ANALISE EXPLORATORIA DE DADOS
+# Visualização da distribuição do número de vítimas
+vitimas_chart = alt.Chart(df).mark_bar().encode(
+    x=alt.X('vitimas', bin=True),
+    y='count()',
+    color=alt.Color('vitimas', scale=alt.Scale(scheme='viridis'))
+).properties(
+    width=600,
+    height=400
+)
+st.altair_chart(vitimas_chart)
